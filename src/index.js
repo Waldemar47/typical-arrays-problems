07af2cad4fcc678a4e609a40f1cc8ee105281d05
Waldemar47 <w.karsten@tut.by>
2020-03-18 12:41:05 +0300

    init: file improvement index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 exports.min = function min (array) {
     if (array == undefined || array.length == 0) return 0;
-    let min = array[0]; 
-    for(let i = 0; i < array.length; i++) {
-      if (array[i] < min) min = array[i]; 
+    let little = array[0]; 
+    for(let a = 0; a < array.length; a++) {
+      if (array[a] < little) little = array[a]; 
     }
-    return min;
+    return little;
   }
   
   exports.max = function max (array) {
     if (array == undefined || array.length == 0) return 0;
-    let max = array[0]; 
-    for(let i = 0; i < array.length; i++) {
-      if (array[i] > max) max = array[i]; 
+    let big = array[0]; 
+    for(let b = 0; b < array.length; b++) {
+      if (array[b] > big) big = array[b]; 
     }
-    return max;
+    return big;
   }
   
   exports.avg = function avg (array) {
     if (array == undefined || array.length == 0) return 0;
-    return array.reduce((a,b) => a + b) / array.length; 
+    return array.reduce((x,y) => x + y) / array.length; 
   }
   
